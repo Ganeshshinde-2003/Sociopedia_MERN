d93de02aa05904234767f4406be4f933b9783e2e
Ganesh <sindheganesh2004@gmail.com>
2023-06-20 21:58:05 +0530

    Project finished

--- client/src/App.js
+++ client/src/App.js
@@ -22,8 +22,7 @@ function App() {
             <Route path="/" element={<LoginPage />} />
             <Route
               path="/home"
-              // element={isAuth ? <HomePage /> : <Navigate to="/" />}
-              element={<HomePage />}
+              element={isAuth ? <HomePage /> : <Navigate to="/" />}
             />
             <Route
               path="/profile/:userId"
